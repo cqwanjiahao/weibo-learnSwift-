e5c248ad7d6af85b45ae5028cdc628d034e606d8
cqwanjiahao <490678868@qq.com>
2017-08-22 11:09:25 +0800

    创建项目:集成pod  storyBoard搭建基础界面

---
+++ weiboLearnSwift/Classes/Features/Login/JHWbLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JHWbLoginViewController.swift
+//  weiboLearnSwift
+//
+//  Created by 万家豪 on 2017/8/22.
+//  Copyright © 2017年 万家豪. All rights reserved.
+//
+
+import UIKit
+
+class JHWbLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
